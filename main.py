fe7b3e4e15804d57bb4525915d7797c1c169845f
tmwatchanan <tmwatchanan@gmail.com>
2017-10-13 11:05:19 +0700

    Save all processed images into files

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import numpy as np
 import cv2
 import re  # Regular Expression
+from skimage.feature import greycomatrix, greycoprops
 
-TRAINING_SET_DIRECTORY = 'Contest\\'
+TRAINING_SET_DIRECTORY = "Contest\\"
+PROCESSED_DIRECTORY = "C:\\Users\\Watchanan\\Desktop\\Processed\\"
 LIST_FILENAME = TRAINING_SET_DIRECTORY + "list.txt"
 RESULT_FILENAME = TRAINING_SET_DIRECTORY + "result.txt"
 
 RESIZE_IMAGE_WIDTH = 400.
+CURRENT_FILENAME = ''
+SAVE_IMAGE_EXTENSION = '.jpg'
 
 
 def ResizeByWidth(image):
@@ -14,6 +18,9 @@ def ResizeByWidth(image):
     imgScale = RESIZE_IMAGE_WIDTH / width
     newX, newY = image.shape[1] * imgScale, image.shape[0] * imgScale
     return cv2.resize(image, (int(newX), int(newY)))
+
+def SaveImage(image, string):
+    cv2.imwrite(PROCESSED_DIRECTORY + CURRENT_FILENAME + '-' + string + SAVE_IMAGE_EXTENSION, image)
 
 
 with open(LIST_FILENAME) as f:
@@ -42,31 +49,41 @@ training_set_list = [x.strip() for x in training_set_list if x.strip()]
 
 # --------------------------------------------------------
 
-IMAGE_FILENAME = '7.jpg'
+for IMAGE_FILENAME in training_set_list:
+    fileName = re.split('\.', IMAGE_FILENAME)
+    CURRENT_FILENAME = fileName[0]
 
-# 01.jpg
-im1_original = cv2.imread(TRAINING_SET_DIRECTORY + IMAGE_FILENAME)
-# cv2.imshow("original " + IMAGE_FILENAME, im1_original)
+    im_original = cv2.imread(TRAINING_SET_DIRECTORY + IMAGE_FILENAME)
+    # cv2.imshow('original', im)
 
-im1 = ResizeByWidth(im1_original)
-cv2.imshow("resized " + IMAGE_FILENAME, im1)
+    im = ResizeByWidth(im_original)
+    # cv2.imshow("resized " + IMAGE_FILENAME, im)
 
-blur = cv2.GaussianBlur(im1, (15, 15), 2)
-hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-h, s, v = cv2.split(im1)
-# s.fill(110)
-# v.fill(255)
-hsv_image = cv2.merge([h, s, v])
-LOWER_YELLOW = np.array([35, 110, 180], dtype=np.uint8)
-UPPER_YELLOW = np.array([90, 255, 255], dtype=np.uint8)
-yellow_mask = cv2.inRange(hsv_image, LOWER_YELLOW, UPPER_YELLOW)
-cv2.imshow('yellow mask (binary)', yellow_mask)
-yellow_res = cv2.bitwise_and(im1, im1, mask= yellow_mask)
-cv2.imshow('yellow mask (color)', yellow_res)
+    blur = cv2.GaussianBlur(im, (15, 15), 2)
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(im)
+    # s.fill(255)
+    # v.fill(255)
+    hsv_image = cv2.merge([h, s, v])
+    LOWER_YELLOW = np.array([16, 120, 100], dtype=np.uint8)
+    UPPER_YELLOW = np.array([95, 255, 255], dtype=np.uint8)
+    yellow_mask = cv2.inRange(hsv_image, LOWER_YELLOW, UPPER_YELLOW)
+    kernel = np.ones((5,5),np.uint8)
+    yellow_mask_opening = cv2.morphologyEx(yellow_mask, cv2.MORPH_OPEN, kernel)
+    yellow_mask_median = cv2.medianBlur(yellow_mask_opening, 5)
+    # cv2.imshow('yellow mask (binary)', yellow_mask)
+    yellow_res = cv2.bitwise_and(im, im, mask=yellow_mask_median)
+    # cv2.imshow('yellow mask (color)', yellow_res)
+    SaveImage(yellow_res, 'yellow_mask')
+
+    # while True:
+    #     if cv2.waitKey(1) & 0xFF == ord('w'):
+    #         break
 
 # --------------------------------------------------------
 
-while True:
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        exit()
+print("DONE")
+# while True:
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         cv2.destroyAllWindows()
+#         exit()
